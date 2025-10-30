855cb45cfe57bf7895549c7d322171d8fa837976
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-10-30 10:57:49 -0300

    helmet constructor

--- myApp.js
+++ myApp.js
@@ -52,22 +52,35 @@ let timeInSeconds = 90*24*60*60
 
 module.exports = app;
 const api = require('./server.js');
-app.use(helmet.frameguard({action: 'deny'})) // Restringe quem pode colocar seu site em um iframe, impedindo ClickJacking
-app.use(helmet.hidePoweredBy()); // Esconde em que tech o site roda (nesse exemplo o Express)
-app.use(helmet.xssFilter()) // Filtra com potenciais vulnerabilidades de XSS. Tem um limite, mas já ajuda
-app.use(helmet.noSniff()) // Faz com que os navegadores não tentem advinhar o Content-type e sobrescrever o mandado
-app.use(helmet.ieNoOpen()) // Impede que os usuários de internet Explorer executem downloads no contexto de sites confiaveis 
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true})) // Recomda o uso de https para sites que rodam tal
-app.use(helmet.dnsPrefetchControl()) // desativa a pré-busca de DNS, perde desempenho, mas ganha segurança
-app.use(helmet.noCache()) // Desativa o cache da página, util para a produção.
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", 'trusted-cdn.com']
-  }
+// app.use(helmet.frameguard({action: 'deny'})) // Restringe quem pode colocar seu site em um iframe, impedindo ClickJacking
+// app.use(helmet.hidePoweredBy()); // Esconde em que tech o site roda (nesse exemplo o Express)
+// app.use(helmet.xssFilter()) // Filtra com potenciais vulnerabilidades de XSS. Tem um limite, mas já ajuda
+// app.use(helmet.noSniff()) // Faz com que os navegadores não tentem advinhar o Content-type e sobrescrever o mandado
+// app.use(helmet.ieNoOpen()) // Impede que os usuários de internet Explorer executem downloads no contexto de sites confiaveis 
+// app.use(helmet.hsts({maxAge: timeInSeconds, force: true})) // Recomda o uso de https para sites que rodam tal
+// app.use(helmet.dnsPrefetchControl()) // desativa a pré-busca de DNS, perde desempenho, mas ganha segurança
+// app.use(helmet.noCache()) // Desativa o cache da página, util para a produção.
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", 'trusted-cdn.com']
+//   }
   
-})
-) // Define uma lista de fontes confiaveis no qual a pagina pode baixar
+// })
+// ) // Define uma lista de fontes confiaveis no qual a pagina pode baixar
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com']
+    }
+  },
+  dnsPrefetchControl: flase
+})) // incluirá automaticamente todos os middlewares apresentados acima, exceto noCache() e contentSecurityPolicy()
+
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
